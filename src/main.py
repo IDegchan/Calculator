36064f200ac68842f06839674582cf54a98c6a73
IDegchan <gamerolikam@gmail.com>
2025-03-30 17:43:16 +0300

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   src/main.py

--- src/main.py
+++ src/main.py
@@ -28,48 +28,12 @@ def button_press(expression, button):
     return expression
 
 
-def on_click(e, display, page):
-    display.value = button_press(display.value, e.control.data)
-    page.update()
-
-
-def create_button(text, display, page, *args, **kwargs):
-    button = ft.ElevatedButton(
-        text=text,
-        data=text,
-        on_click=lambda e: on_click(e, display, page),
-        width=80,
-        height=80,
-        *args,
-        **kwargs,
-    )
-    if text == "B":
-        button.content = ft.Icon(
-            name=ft.Icons.BACKSPACE, color=ft.Colors.BLACK, size=18
-        )
-        button.text = None
-    elif text == "R":
-        button.content = ft.Icon(name=ft.Icons.REPLAY, color=ft.Colors.WHITE, size=18)
-        button.text = None
-
-    if text in ("+", "-", "*", "/", "="):
-        button.bgcolor = ft.Colors.ORANGE
-        button.color = ft.Colors.WHITE
-    elif text in ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", "B"):
-        button.bgcolor = ft.Colors.WHITE
-        button.color = ft.Colors.BLACK
-    else:
-        button.bgcolor = ft.Colors.GREY
-        button.color = ft.Colors.WHITE
-    return button
-
-
-def main(page: ft.Page):
+def calculator(page: ft.Page):
     page.title = "Калькулятор"
     page.theme_mode = ft.ThemeMode.LIGHT
     page.window.resizable = False
     page.window.width = 400
-    page.window.height = 675
+    page.window.height = 650
     display = ft.Text(value="0", text_align=ft.TextAlign.RIGHT, max_lines=1, size=30)
     buttons = [
         ["AC", "R", "^", "/"],
@@ -79,23 +43,107 @@ def main(page: ft.Page):
         ["0", ".", "B", "="],
     ]
 
-    page.appbar = ft.AppBar(title=ft.Text("Калькулятор", size=24, weight="bold"))
+    def on_click(e, display, page):
+        display.value = button_press(display.value, e.control.data)
+        page.update()
 
-    page.add(
-        ft.Column(
-            [display, ft.Divider(height=10)]
-            + [
-                ft.Row(
-                    [create_button(char, display, page) for char in row],
-                    alignment=ft.MainAxisAlignment.CENTER,
-                    wrap=False,
-                )
-                for row in buttons
-            ],
-            alignment=ft.MainAxisAlignment.CENTER,
+    def create_button(text, display, page, *args, **kwargs):
+        button = ft.ElevatedButton(
+            text=text,
+            data=text,
+            on_click=lambda e: on_click(e, display, page),
+            width=80,
+            height=80,
+            *args,
+            **kwargs,
         )
-    )
+        if text == "B":
+            button.content = ft.Icon(
+                name=ft.Icons.BACKSPACE, color=ft.Colors.BLACK, size=18
+            )
+            button.text = None
+        elif text == "R":
+            button.content = ft.Icon(
+                name=ft.Icons.REPLAY, color=ft.Colors.WHITE, size=18
+            )
+            button.text = None
+
+        if text in ("+", "-", "*", "/", "="):
+            button.bgcolor = ft.Colors.ORANGE
+            button.color = ft.Colors.WHITE
+        elif text in ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", "B"):
+            button.bgcolor = ft.Colors.WHITE
+            button.color = ft.Colors.BLACK
+        else:
+            button.bgcolor = ft.Colors.GREY
+            button.color = ft.Colors.WHITE
+        return button
+
+    def route_change(route):
+        page.views.clear()
+        change2 = ft.IconButton(
+            icon=ft.Icons.CALCULATE,
+            on_click=lambda _: page.go("/advenced"),
+            bgcolor=ft.Colors.WHITE,
+            icon_color=ft.Colors.BLACK,
+        )
+        page.views.append(
+            ft.View(
+                "/",
+                [
+                    ft.AppBar(
+                        title=ft.Text("Калькулятор", size=22, weight="bold"),
+                        actions=[change2],
+                    ),
+                    ft.Column(
+                        [display, ft.Divider(height=10)]
+                        + [
+                            ft.Row(
+                                [create_button(char, display, page) for char in row],
+                                alignment=ft.MainAxisAlignment.CENTER,
+                                wrap=False,
+                            )
+                            for row in buttons
+                        ],
+                        alignment=ft.MainAxisAlignment.CENTER,
+                    ),
+                ],
+            )
+        )
+        if page.route == "/advenced":
+            page.views.append(
+                ft.View(
+                    "/advenced",
+                    [
+                        ft.AppBar(
+                            title=ft.Text("Просунутий Калькулятор", size=22, weight="bold"),
+                        ),
+                        ft.Column(
+                            [display, ft.Divider(height=10)]
+                            + [
+                                ft.Row(
+                                    [create_button(char, display, page) for char in row],
+                                    alignment=ft.MainAxisAlignment.CENTER,
+                                    wrap=False,
+                                )
+                                for row in buttons
+                            ],
+                            alignment=ft.MainAxisAlignment.CENTER,
+                        ),
+                    ],
+                )
+            )
+        page.update()
+
+    def view_pop(view):
+        page.views.pop()
+        top_view = page.views[-1]
+        page.go(top_view.route)
+
+    page.on_route_change = route_change
+    page.on_view_pop = view_pop
+    page.go(page.route)
 
 
 if __name__ == "__main__":
-    ft.app(target=main)
+    ft.app(target=calculator)
